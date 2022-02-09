da2eb6ef8e560ad33968184b7d5518421ac2a0b7
Paula Fahmy <paula@nacitadrive.com>
2022-02-09 16:03:42 +0200

    Add project files.

---
+++ TryingOutAuth0/Auth/HasScopeHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TryingOutAuth0.Auth
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    {
+        const string SCOPE_CLAIM_TYPE = "scope";
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+        {
+            // If user does not have the scope claim, get out of here
+            if (!RequestHasCorrectIssuer(context, requirement.Issuer))
+                return Task.CompletedTask;
+
+            var scopes = GetScopes(context, requirement.Issuer);
+
+            // Succeed if the scope array contains the required scope
+            if (ScopesContainRequiredScope(requirement.Scope, scopes))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+
+        private static bool ScopesContainRequiredScope(string requiredScope, string[] scopes) => 
+            scopes.Any(s => s == requiredScope);
+
+        private static string[] GetScopes(AuthorizationHandlerContext context, string requiredIssuer) =>
+            // Split the scopes string into an array
+            context.User.FindFirst(c => c.Type == SCOPE_CLAIM_TYPE && c.Issuer == requiredIssuer).Value.Split(' ');
+
+        private static bool RequestHasCorrectIssuer(AuthorizationHandlerContext context, string requiredIssuer) => 
+            context.User.HasClaim(c => c.Type == SCOPE_CLAIM_TYPE && c.Issuer == requiredIssuer);
+    }
+}
---
+++ TryingOutAuth0/Startup.cs
@@ -0,0 +1,114 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Security.Claims;
+using TryingOutAuth0.Auth;
+
+namespace TryingOutAuth0
+{
+    public class Startup
+    {
+        public const string ACCESS_APPLICATION_SCOPE = "access:weatherforecastapp";
+        public const string READ_WEATHER_FORECAST_SCOPE = "read:weatherforecast";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            string domain = $"https://{Configuration["Auth0:Domain"]}/";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Auth0:ApiIdentifier"];
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = ClaimTypes.NameIdentifier
+                    
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(ACCESS_APPLICATION_SCOPE, policy => policy.Requirements.Add(new HasScopeRequirement(ACCESS_APPLICATION_SCOPE, domain)));
+                options.AddPolicy(READ_WEATHER_FORECAST_SCOPE, policy => policy.Requirements.Add(new HasScopeRequirement(READ_WEATHER_FORECAST_SCOPE, domain)));
+            });
+
+            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TryingOutAuth0", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TryingOutAuth0 v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers().RequireAuthorization(ACCESS_APPLICATION_SCOPE);
+            });
+        }
+    }
+}
