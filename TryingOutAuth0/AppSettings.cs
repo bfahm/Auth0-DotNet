6e5615082f27e68db14b26a5be8f5e72ab0982dc
Paula Fahmy <paula@nacitadrive.com>
2022-02-10 15:43:42 +0200

    Added appsettings and user secrets

---
+++ TryingOutAuth0/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace TryingOutAuth0
+{
+    public class AppSettings
+    {
+        public Auth0 Auth0 { get; set; }
+    }
+
+    public class Auth0
+    {
+        public string Domain { get; set; }
+        public string ApiIdentifier { get; set; }
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
+    }
+}
