a0c21167396d6e6a23330226bd091b00065dcc32
Paula Fahmy <paula@nacitadrive.com>
2022-02-10 15:44:48 +0200

    Added login and get user info endpoints

---
+++ TryingOutAuth0/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+using TryingOutAuth0.Auth.Auth0Client;
+using TryingOutAuth0.ViewModels;
+
+namespace TryingOutAuth0.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthClientService _authClientService;
+
+        public AuthController(AuthClientService authClientService)
+        {
+            _authClientService = authClientService;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginRequestViewModel request)
+        {
+            var test = await _authClientService.LoginAsync(request.Email, request.Password);
+            return Ok(test);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Email()
+        {
+            var accessToken = Request.Headers[HeaderNames.Authorization].ToString().Split(" ")[1] ?? "";
+            var test = await _authClientService.GetEmailAsync(accessToken);
+            return Ok(test);
+        }
+    }
+}
---
+++ TryingOutAuth0/ViewModels/LoginRequestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TryingOutAuth0.ViewModels
+{
+    public class LoginRequestViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
